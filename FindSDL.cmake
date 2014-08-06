4b666eeb4313c9003b96d4039262168e25007005
Walter Gray <wgray@leapmotion.com>
2014-08-06 11:38:13 -0700

    Fixes for SDL find behavior on mac

--- FindSDL.cmake
+++ FindSDL.cmake
@@ -116,8 +116,69 @@
 # (To distribute this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
-#set(_likely_folders SDL SDL12 SDL11 SDL2)
-include(LeapFindModuleHelpers)
+function(find_multitype_library shared_out static_out import_out)
+  list(REMOVE_AT ARGV 0) #remove shared_out
+  list(REMOVE_AT ARGV 0) #remove static_out
+  list(REMOVE_AT ARGV 0) #remove import_out
+
+  set(_oldlibsuffixes "${CMAKE_FIND_LIBRARY_SUFFIXES}")
+    set(CMAKE_FIND_LIBRARY_SUFFIXES "${CMAKE_SHARED_LIBRARY_SUFFIX}")
+    find_library(${shared_out} ${ARGV})
+    set(CMAKE_FIND_LIBRARY_SUFFIXES "${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    find_library(${static_out} ${ARGV})
+    set(CMAKE_FIND_LIBRARY_SUFFIXES "${CMAKE_IMPORT_LIBRARY_SUFFIX}")
+    find_library(${import_out} ${ARGV})
+  set(CMAKE_FIND_LIBRARY_SUFFIXES "${_oldlibsuffixes}")
+
+  #TODO:verify the types of the static & import libraries
+  if(MSVC)
+    #file(READ ${static_out})
+  endif()
+
+endfunction()
+
+#Checks <namespace>_SHARED_LIB, <namespace>_STATIC_LIB and <namespace>_IMPORT_LIB
+#And fills <namespace>_LIBRARY with the appropriate lib type depending on which is found
+#If both are found, it will default to shared.  We may, at a later time, also add verification
+#of if the static library is actually a static lib and not an import lib on windows.
+#It will then fill <namespace>_LIBRARY_TYPE with either SHARED or STATIC
+function(select_library_type namespace)
+  #select the primary library type
+  if(${namespace}_SHARED_LIB AND EXISTS "${${namespace}_SHARED_LIB}")
+    #add either the .lib or the .dylib to the libraries list
+    if(${namespace}_IMPORT_LIB AND EXISTS "${${namespace}_IMPORT_LIB}")
+      set(${namespace}_LIBRARIES "${${namespace}_LIBRARIES}" "${${namespace}_IMPORT_LIB}" PARENT_SCOPE)
+    else()
+      set(${namespace}_LIBRARIES "${${namespace}_LIBRARIES}" "${${namespace}_SHARED_LIB}" PARENT_SCOPE)
+    endif()
+
+    set(${namespace}_LIBRARY "${${namespace}_SHARED_LIB}" PARENT_SCOPE)
+    set(${namespace}_LIBRARY_TYPE "SHARED" PARENT_SCOPE)
+  elseif(${namespace}_STATIC_LIB AND EXISTS "${${namespace}_STATIC_LIB}")
+    set(${namespace}_LIBRARIES "${${namespace}_LIBRARIES}" "${${namespace}_STATIC_LIB}" PARENT_SCOPE)
+    set(${namespace}_LIBRARY "${${namespace}_STATIC_LIB}" PARENT_SCOPE)
+    set(${namespace}_LIBRARY_TYPE "STATIC" PARENT_SCOPE)
+  endif()
+
+endfunction()
+
+function(find_likely_folders package folder_list_var path_list )
+  list(REMOVE_AT ARGV 0) #pop package name
+  list(REMOVE_AT ARGV 0) #pop folder_list_var
+
+  set(_folders ${${folder_list_var}}) #make sure we're appending
+
+  foreach(_path ${ARGV})
+    file(GLOB _subdirs RELATIVE ${_path} ${_path}/*)
+    foreach(_subdir ${_subdirs})
+      if(IS_DIRECTORY ${_path}/${_subdir} AND _subdir MATCHES "^${package}*")
+        list(APPEND _folders ${_path}/${_subdir})
+      endif()
+    endforeach()
+  endforeach()
+
+  set(${folder_list_var} "${_folders}" PARENT_SCOPE)
+endfunction()
 
 function(sdl_parse_version_file filename major minor patch version_string)
   file(STRINGS "${filename}" _major_line REGEX "^#define[ \t]+SDL_MAJOR_VERSION[ \t]+[0-9]+$")
@@ -136,6 +197,9 @@ function(sdl_parse_version_file filename major minor patch version_string)
   set(${version_string} "${sdl_major}.${sdl_minor}.${sdl_patch}" PARENT_SCOPE)
 endfunction()
 
+#############################
+# The actual find module begins here
+#############################
 if(NOT EXISTS SDL_ROOT_DIR)
 
   set(_likely_folders "")
@@ -178,10 +242,9 @@ if(NOT EXISTS SDL_ROOT_DIR)
 
   find_path(SDL_ROOT_DIR
     NAMES include/SDL_Version.h 
-          include/SDL${SDL_MAJOR_VERSION}/SDL_Version.h 
+          include/SDL2/SDL_Version.h 
           include/SDL/SDL_version.h 
     HINTS $ENV{SDLDIR} ${_ok_folders}
-    
   )
   
 endif()
