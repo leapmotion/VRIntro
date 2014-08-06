5f98af4c5322a092070006a7f84bd43c10999ab2
Walter Gray <wgray@leapmotion.com>
2014-08-06 16:59:19 -0700

    Fixed interface include dirs and compile definitions for SFML::Main

--- FindSFML.cmake
+++ FindSFML.cmake
@@ -280,8 +280,13 @@ foreach(_component ${SFML_FIND_COMPONENTS})
 
   if(_componentLOWER STREQUAL "main") #main is always a static lib
     generate_import_target(SFML_MAIN STATIC TARGET SFML::Main)
+    set_property(TARGET SFML::Main APPEND PROPERTY INTERFACE_COMPILE_DEFINITIONS ${SFML_DEFINITIONS})
+    set_property(TARGET SFML::Main APPEND PROPERTY INTEFACE_INCLUDE_DIRECTORIES ${SFML_INCLUDE_DIR})
+  
   else()
     generate_import_target(SFML_${_componentUPPER} ${_libtype} TARGET SFML::${_componentCap})
   endif()
+
   set_property(TARGET SFML::SFML APPEND PROPERTY INTERFACE_LINK_LIBRARIES SFML::${_componentCap})
 endforeach()
+
