67e5e60a0c377ddb67edfaaa211a45b8e140f9fb
Chen Zheng <czheng@leapmotion.com>
2014-09-26 09:37:35 -0700

    Adding TargetStrip.cmake for stripping symbols

---
+++ TargetStrip.cmake
@@ -0,0 +1,15 @@
+# primary function of this macro: strip symbols at link time on Mac and Linux
+# secondary function: avoid MSVC warnings in Debug by specifying /nodefaultlib
+# tertiary function: avoid MSVC warnings about combining /incremental with /ltcg
+macro(TARGET_STRIP Target)
+  set(STRIPPED_EXE_LINKER_FLAGS "-Xlinker -unexported_symbol -Xlinker \"*\" -Xlinker -dead_strip -Xlinker -dead_strip_dylibs")
+  if(MSVC)
+    if(USE_PRODUCTION_FLAGS)
+      set_target_properties(${Target} PROPERTIES LINK_FLAGS_DEBUG "/INCREMENTAL:NO /NODEFAULTLIB:MSVCRT")
+    else()
+      set_target_properties(${Target} PROPERTIES LINK_FLAGS_DEBUG "/NODEFAULTLIB:MSVCRT")
+    endif()
+  else()
+    set_target_properties(${Target} PROPERTIES LINK_FLAGS_RELEASE ${STRIPPED_EXE_LINKER_FLAGS})
+  endif()
+endmacro()
