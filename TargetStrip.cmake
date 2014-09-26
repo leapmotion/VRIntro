e44bb3b6c7bc0d86b4ab11934885a775cb6a2265
Chen Zheng <czheng@leapmotion.com>
2014-09-26 10:14:50 -0700

    Getting rid of the variable

--- TargetStrip.cmake
+++ TargetStrip.cmake
@@ -2,7 +2,6 @@
 # secondary function: avoid MSVC warnings in Debug by specifying /nodefaultlib
 # tertiary function: avoid MSVC warnings about combining /incremental with /ltcg
 macro(TARGET_STRIP Target)
-  set(STRIPPED_EXE_LINKER_FLAGS "-Xlinker -unexported_symbol -Xlinker \"*\" -Xlinker -dead_strip -Xlinker -dead_strip_dylibs")
   if(MSVC)
     if(CMAKE_CXX_FLAGS_RELEASE)
       set_target_properties(${Target} PROPERTIES LINK_FLAGS_DEBUG "/INCREMENTAL:NO /NODEFAULTLIB:MSVCRT")
@@ -10,6 +9,7 @@ macro(TARGET_STRIP Target)
       set_target_properties(${Target} PROPERTIES LINK_FLAGS_DEBUG "/NODEFAULTLIB:MSVCRT")
     endif()
   else()
-    set_target_properties(${Target} PROPERTIES LINK_FLAGS_RELEASE ${STRIPPED_EXE_LINKER_FLAGS})
+    set_target_properties(${Target} PROPERTIES LINK_FLAGS_RELEASE 
+		"-Xlinker -unexported_symbol -Xlinker \"*\" -Xlinker -dead_strip -Xlinker -dead_strip_dylibs")
   endif()
 endmacro()
