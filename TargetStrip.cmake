1cb0ab90ea5141c736e4c80f4b0667b1912c8613
Walter Gray <wgray@leapmotion.com>
2014-10-07 15:37:20 -0700

    fixed capitalization of the command
    changed from macro to function

--- TargetStrip.cmake
+++ TargetStrip.cmake
@@ -1,7 +1,7 @@
 # primary function of this macro: strip symbols at link time on Mac and Linux
 # secondary function: avoid MSVC warnings in Debug by specifying /nodefaultlib
 # tertiary function: avoid MSVC warnings about combining /incremental with /ltcg
-macro(TARGET_STRIP Target)
+function(target_strip Target)
   if(MSVC)
     if(CMAKE_CXX_FLAGS_RELEASE)
       set_target_properties(${Target} PROPERTIES LINK_FLAGS_DEBUG "/INCREMENTAL:NO /NODEFAULTLIB:MSVCRT")
@@ -12,4 +12,4 @@ macro(TARGET_STRIP Target)
     set_target_properties(${Target} PROPERTIES LINK_FLAGS_RELEASE 
 		"-Xlinker -unexported_symbol -Xlinker \"*\" -Xlinker -dead_strip -Xlinker -dead_strip_dylibs")
   endif()
-endmacro()
+endfunction()
