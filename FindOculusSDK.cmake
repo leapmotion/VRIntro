fe42c83f920d52390634c74a70bd772233e6f0bb
Jonathan Marsden <jmarsden@leapmotion.com>
2014-10-08 09:56:17 -0700

    Find OculusSDK for Mac

--- FindOculusSDK.cmake
+++ FindOculusSDK.cmake
@@ -10,10 +10,10 @@
 #
 # Variables
 # ^^^^^^^^^
-#   Oculus_ROOT_DIR
-#   Oculus_FOUND
-#   Oculus_INCLUDE_DIR
-#   Oculus_LIBRARIES
+#   OculusSDK_ROOT_DIR
+#   OculusSDK_INCLUDE_DIR
+#   OculusSDK_LIBRARIES_RELEASE
+#   OculusSDK_LIBRARIES_DEBUG
 #
 
 
@@ -24,6 +24,9 @@ set(OculusSDK_INCLUDE_DIR ${OculusSDK_ROOT_DIR}/LibOVR/Include)
 if(MSVC)
   find_library(OculusSDK_LIBRARY_RELEASE "libovr.lib" HINTS "${OculusSDK_ROOT_DIR}/LibOVR/Lib/Win32/VS2013" PATH_SUFFIXES lib)
   find_library(OculusSDK_LIBRARY_DEBUG "libovrd.lib" HINTS "${OculusSDK_ROOT_DIR}/LibOVR/Lib/Win32/VS2013" PATH_SUFFIXES lib)
+elseif(APPLE)
+  find_library(OculusSDK_LIBRARY_RELEASE "libovr.a" HINTS "${OculusSDK_ROOT_DIR}/LibOVR/Lib/Mac" PATH_SUFFIXES Release)
+  find_library(OculusSDK_LIBRARY_DEBUG "libovr.a" HINTS "${OculusSDK_ROOT_DIR}/LibOVR/Lib/Mac" PATH_SUFFIXES Debug)
 else()
   # Linux's oculus-1.9.0 package's libs are in lib64
   find_library(OculusSDK_LIBRARY_RELEASE "libOCULUS.a" HINTS "${OculusSDK_ROOT_DIR}" PATH_SUFFIXES lib lib64)
