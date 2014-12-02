3255c7d067ba793366ff91daa74cbc626b23fec9
Walter Gray <wgray@leapmotion.com>
2014-12-02 12:36:34 -0800

    made protobuf find module search for protobuf<version>-libc++ if USE_LIBCXX

--- FindProtobuf.cmake
+++ FindProtobuf.cmake
@@ -1,10 +1,14 @@
 include(CreateImportTargetHelpers)
 
+if(USE_LIBCXX)
+ 	set(_suffix -libc++)
+ endif()
+
 find_path(Protobuf_ROOT_DIR
   NAMES include/google/protobuf/descriptor.h
   HINTS ${EXTERNAL_LIBRARY_DIR}
-  PATH_SUFFIXES protobuf-${Protobuf_FIND_VERSION}
-                protobuf)
+  PATH_SUFFIXES protobuf-${Protobuf_FIND_VERSION}${_suffix}
+                protobuf${_suffix})
 
 set(Protobuf_INCLUDE_DIR ${Protobuf_ROOT_DIR}/include CACHE STRING "")
 
