79604df704be8947d96dac06433c6153b4647524
Walter Gray <wgray@leapmotion.com>
2014-12-02 13:04:15 -0800

    debug test commit

--- FindProtobuf.cmake
+++ FindProtobuf.cmake
@@ -1,9 +1,18 @@
 include(CreateImportTargetHelpers)
 
-if(USE_LIBCXX)
- 	set(_suffix -libc++)
+message(USE_LIBCXX=${USE_LIBCXX})
+set(_suffix "")
+
+if(${USE_LIBCXX})
+	message("Setting protobuf suffix")
+ 	set(_suffix "-libc++")
  endif()
 
+unset(Protobuf_ROOT_DIR CACHE)
+unset(Protobuf_LIBRARY CACHE)
+unset(Protobuf_LIBRARY_DEBUG CACHE)
+
+message(_suffix=${_suffix})
 find_path(Protobuf_ROOT_DIR
   NAMES include/google/protobuf/descriptor.h
   HINTS ${EXTERNAL_LIBRARY_DIR}
@@ -11,13 +20,16 @@ find_path(Protobuf_ROOT_DIR
                 protobuf${_suffix})
 
 set(Protobuf_INCLUDE_DIR ${Protobuf_ROOT_DIR}/include CACHE STRING "")
+message(CMAKE_FIND_LIBRARY_SUFFIXES=${CMAKE_FIND_LIBRARY_SUFFIXES})
 
 
 find_library(Protobuf_LIBRARY
   NAMES libprotobuf
-  HINTS ${Protobuf_ROOT_DIR}/lib/Release
-  		${Protobuf_ROOT_DIR}/lib
+  HINTS "${Protobuf_ROOT_DIR}/lib"
+  		"${Protobuf_ROOT_DIR}/lib/Release"
 )
+message(Protobuf_LIBRARY=${Protobuf_LIBRARY})
+message(searchedin="${Protobuf_ROOT_DIR}/lib")
 
 find_library(Protobuf_LIBRARY_DEBUG
   NAMES libprotobuf
