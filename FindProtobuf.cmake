6ce84749b0b9cd4c15efc54104c3619b02b0752e
Walter Gray <wgray@leapmotion.com>
2014-12-02 12:51:35 -0800

    Fixed non-windows protobuf search path.

--- FindProtobuf.cmake
+++ FindProtobuf.cmake
@@ -12,18 +12,20 @@ find_path(Protobuf_ROOT_DIR
 
 set(Protobuf_INCLUDE_DIR ${Protobuf_ROOT_DIR}/include CACHE STRING "")
 
+
+find_library(Protobuf_LIBRARY
+  NAMES libprotobuf
+  HINTS ${Protobuf_ROOT_DIR}/lib/Release
+  		${Protobuf_ROOT_DIR}/lib
+)
+
 find_library(Protobuf_LIBRARY_DEBUG
   NAMES libprotobuf
   HINTS ${Protobuf_ROOT_DIR}/lib/Debug
 )
 
-find_library(Protobuf_LIBRARY_RELEASE
-  NAMES libprotobuf
-  HINTS ${Protobuf_ROOT_DIR}/lib/Release
-)
-
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Protobuf DEFAULT_MSG
-    Protobuf_LIBRARY_RELEASE Protobuf_LIBRARY_DEBUG Protobuf_INCLUDE_DIR)
+    Protobuf_LIBRARY Protobuf_INCLUDE_DIR)
 
 generate_import_target(Protobuf STATIC)
