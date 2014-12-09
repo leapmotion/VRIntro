7ac53eb35d986734e905639047abf06415bf9d58
Walter Gray <wgray@leapmotion.com>
2014-12-08 19:22:15 -0800

    Made FindHalide generate a shared target on windows.

--- FindHalide.cmake
+++ FindHalide.cmake
@@ -15,6 +15,8 @@
 #   Halide_FOUND
 #   Halide_INCLUDE_DIR
 #   Halide_LIBRARY
+#   Halide_SHARED_LIB
+#   Halide_IMPORT_LIB
 
 find_path(Halide_ROOT_DIR
           NAMES include/Halide.h
@@ -23,14 +25,22 @@ find_path(Halide_ROOT_DIR
 find_path( Halide_INCLUDE_DIR
            NAMES Halide.h
            HINTS ${Halide_ROOT_DIR}
-           PATH_SUFFIXES include    
-           NO_DEFAULT_PATH 
+           PATH_SUFFIXES include
+           NO_DEFAULT_PATH
 )
 
 find_library(Halide_LIBRARY Halide HINTS "${Halide_ROOT_DIR}" PATH_SUFFIXES bin)
+if(WIN32)
+  set(Halide_IMPORT_LIB ${Halide_LIBRARY})
+	find_library(Halide_SHARED_LIB Halide.dll "$[Halide_ROOT_DIR}" PATH_SUFFIXES bin)
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Halide DEFAULT_MSG Halide_INCLUDE_DIR Halide_LIBRARY)
 
 include(CreateImportTargetHelpers)
-generate_import_target(Halide STATIC)
+if(WIN32)
+	generate_import_target(Halide SHARED)
+else()
+	generate_import_target(Halide STATIC)
+endif()
