ffb96dc1faf60d5c48b2635583d3700f01789121
Walter Gray <wgray@leapmotion.com>
2014-12-09 16:35:43 -0800

    Fixed FindHalide to locate the shared library files correctly

--- FindHalide.cmake
+++ FindHalide.cmake
@@ -3,7 +3,9 @@
 # ------------
 #
 # Created by Walter Gray
-# Locate and configure Halide
+# Locate and configure Halide.
+# On win32, it will be configured as a dynamic library since that's how it's
+# distributed.
 #
 # Interface Targets
 # ^^^^^^^^^^^^^^^^^
@@ -15,8 +17,10 @@
 #   Halide_FOUND
 #   Halide_INCLUDE_DIR
 #   Halide_LIBRARY
+#   Halide_STATIC_LIB
 #   Halide_SHARED_LIB
 #   Halide_IMPORT_LIB
+#   Halide_LIBRARY_TYPE
 
 find_path(Halide_ROOT_DIR
           NAMES include/Halide.h
@@ -29,19 +33,32 @@ find_path( Halide_INCLUDE_DIR
            NO_DEFAULT_PATH
 )
 
-find_library(Halide_LIBRARY Halide HINTS "${Halide_ROOT_DIR}" PATH_SUFFIXES bin)
 if(WIN32)
-  set(Halide_IMPORT_LIB ${Halide_LIBRARY})
-  find_library(Halide_SHARED_LIB Halide.dll "$[Halide_ROOT_DIR}" PATH_SUFFIXES bin)
-  mark_as_advanced(Halide_SHARED_LIB)
+  set(Halide_LIBRARY_TYPE SHARED)
+else()
+  set(Halide_LIBRARY_TYPE STATIC)
+endif()
+
+if(${Halide_LIBRARY_TYPE} STREQUAL STATIC)
+  find_library(Halide_STATIC_LIB Halide HINTS "${Halide_ROOT_DIR}" PATH_SUFFIXES bin)
+  mark_as_advanced(Halide_STATIC_LIB)
+  set(Halide_LIBRARY ${Halide_STATIC_LIB})
+else()
+  if(WIN32)
+    find_library(Halide_IMPORT_LIB Halide HINTS "${Halide_ROOT_DIR}" PATH_SUFFIXES bin)
+
+    list(INSERT CMAKE_FIND_LIBRARY_SUFFIXES 0 .dll)
+    find_library(Halide_SHARED_LIB Halide HINTS "${Halide_ROOT_DIR}" PATH_SUFFIXES bin)
+    list(REMOVE_AT CMAKE_FIND_LIBRARY_SUFFIXES 0)
+  else()
+    message(FATAL_ERROR "Halide find module not implemented for shared config on non-win32 platforms")
+  endif()
+
+  mark_as_advanced(Halide_SHARED_LIB Halide_IMPORT_LIB)
+  set(Halide_LIBRARY ${Halide_SHARED_LIB})
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Halide DEFAULT_MSG Halide_INCLUDE_DIR Halide_LIBRARY)
+find_package_handle_standard_args(Halide DEFAULT_MSG Halide_ROOT_DIR Halide_INCLUDE_DIR Halide_LIBRARY)
 
-include(CreateImportTargetHelpers)
-if(WIN32)
-	generate_import_target(Halide SHARED)
-else()
-	generate_import_target(Halide STATIC)
-endif()
+generate_import_target(Halide ${Halide_LIBRARY_TYPE})
