c63564a16d8865f480452d21384c17d0f28a15e0
Walter Gray <wgray@leapmotion.com>
2014-12-12 15:51:35 -0800

    Added a helpful add_halide_generator function to the FIndHalide module.  Useful for generating AoT files from simple generators.

--- FindHalide.cmake
+++ FindHalide.cmake
@@ -7,6 +7,14 @@
 # On win32, it will be configured as a dynamic library since that's how it's
 # distributed.
 #
+# Helper Functions
+# ^^^^^^^^^^^^^^^^
+# add_halide_generator(sourcevar generator_file aot_file_root) 
+#  Given a .cpp file with a main function that takes as an argument the name of the 
+#  Halide AoT .h/.o file pair to generate, compiles and runs said program and outputs
+#  the file pair to the current CMAKE_BINARY_DIR. It then appends the full path to
+#  aot_file_root.h and aot_file_root.o to sourcevar.
+#  
 # Interface Targets
 # ^^^^^^^^^^^^^^^^^
 #   Halide::Halide
@@ -64,3 +72,29 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Halide DEFAULT_MSG Halide_ROOT_DIR Halide_INCLUDE_DIR Halide_LIBRARY)
 
 generate_import_target(Halide ${Halide_LIBRARY_TYPE})
+
+function(add_halide_generator sourcevar generator_file aot_file)
+  if(NOT WIN32)
+    set(_compile_flags "-std=c++11")
+    set(_link_flags "-lz")
+  else()
+    set(_link_flags "/STACK:8388608,1048576")
+  endif()
+
+  get_filename_component(_filepath ${generator_file} ABSOLUTE)
+  
+  #This isn't exactly ideal - add_custom_command would be better but I'm not sure
+  #how to make it compile and run a thing 
+  try_run(_run_result _compile_result ${CMAKE_BINARY_DIR} ${_filepath}
+    CMAKE_FLAGS "-DCMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD=c++11 -DCMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY=libc++"
+    LINK_LIBRARIES Halide::Halide ${_link_flags}
+    COMPILE_DEFINITIONS ${_compile_flags}
+    COMPILE_OUTPUT_VARIABLE _compile_output
+    RUN_OUTPUT_VARIABLE _run_output
+    ARGS ${aot_file}
+  )
+
+  set(${sourcevar} ${sourcevar} ${CMAKE_BINARY_DIR}/${aot_file}.h ${CMAKE_BINARY_DIR}/${aot_file}.o)
+  #message("compile=${_compile_result},${_compile_output}")
+  #message("run=${_run_result},${_run_output}")
+endfunction()
