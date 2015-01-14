76900164a0e0a0e365e510112a0a53fe6ee81070
Jimmy He <jhe@leapmotion.com>
2015-01-13 23:54:29 -0800

    Fix FindBullet.cmake for CMake 3.1

--- cmake-module/FindBullet.cmake
+++ cmake-module/FindBullet.cmake
@@ -29,6 +29,22 @@ if(MSVC)
   find_library(Bullet_COLLISION_LIBRARY_DEBUG NAMES "BulletCollision_Debug.lib" HINTS "${Bullet_ROOT_DIR}/lib/Win32/VS2013/Debug" PATH_SUFFIXES lib)
   find_library(Bullet_DYNAMICS_LIBRARY_DEBUG NAMES "BulletDynamics_Debug.lib" HINTS "${Bullet_ROOT_DIR}/lib/Win32/VS2013/Debug" PATH_SUFFIXES lib)
   find_library(Bullet_LINEAR_MATH_LIBRARY_DEBUG NAMES "LinearMath_Debug.lib" HINTS "${Bullet_ROOT_DIR}/lib/Win32/VS2013/Debug" PATH_SUFFIXES lib)
+
+  add_library(Bullet::Collision STATIC IMPORTED)
+  set_target_properties(Bullet::Collision PROPERTIES IMPORTED_LOCATION_DEBUG ${Bullet_COLLISION_LIBRARY_DEBUG})
+  set_target_properties(Bullet::Collision PROPERTIES IMPORTED_LOCATION_RELEASE ${Bullet_COLLISION_LIBRARY_RELEASE})
+
+  add_library(Bullet::Dynamics STATIC IMPORTED)
+  set_target_properties(Bullet::Dynamics PROPERTIES IMPORTED_LOCATION_DEBUG ${Bullet_DYNAMICS_LIBRARY_DEBUG})
+  set_target_properties(Bullet::Dynamics PROPERTIES IMPORTED_LOCATION_RELEASE ${Bullet_DYNAMICS_LIBRARY_RELEASE})
+
+  add_library(Bullet::LinearMath STATIC IMPORTED)
+  set_target_properties(Bullet::LinearMath PROPERTIES IMPORTED_LOCATION_DEBUG ${Bullet_LINEAR_MATH_LIBRARY_DEBUG})
+  set_target_properties(Bullet::LinearMath PROPERTIES IMPORTED_LOCATION_RELEASE ${Bullet_LINEAR_MATH_LIBRARY_RELEASE})
+
+  add_library(Bullet::Bullet INTERFACE IMPORTED GLOBAL)
+  target_link_libraries(Bullet::Bullet INTERFACE Bullet::Collision Bullet::Dynamics Bullet::LinearMath)
+  set_target_properties(Bullet::Bullet PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${Bullet_INCLUDE_DIR})
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin") # Mac
   find_library(Bullet_COLLISION_LIBRARY_RELEASE NAMES "BulletCollision.a" HINTS "${Bullet_ROOT_DIR}/lib/Mac/Release" PATH_SUFFIXES lib)
   find_library(Bullet_DYNAMICS_LIBRARY_RELEASE NAMES "BulletDynamics.a" HINTS "${Bullet_ROOT_DIR}/lib/Mac/Release" PATH_SUFFIXES lib)
@@ -37,12 +53,15 @@ elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin") # Mac
   find_library(Bullet_COLLISION_LIBRARY_DEBUG NAMES "BulletCollision_Debug.a" HINTS "${Bullet_ROOT_DIR}/lib/Mac/Debug" PATH_SUFFIXES lib)
   find_library(Bullet_DYNAMICS_LIBRARY_DEBUG NAMES "BulletDynamics_Debug.a" HINTS "${Bullet_ROOT_DIR}/lib/Mac/Debug" PATH_SUFFIXES lib)
   find_library(Bullet_LINEAR_MATH_LIBRARY_DEBUG NAMES "LinearMath_Debug.a" HINTS "${Bullet_ROOT_DIR}/lib/Mac/Debug" PATH_SUFFIXES lib)
+
+  set(Bullet_LIBRARY_RELEASE "${Bullet_COLLISION_LIBRARY_RELEASE}" "${Bullet_DYNAMICS_LIBRARY_RELEASE}" "${Bullet_LINEAR_MATH_LIBRARY_RELEASE}")
+  set(Bullet_LIBRARY_DEBUG "${Bullet_COLLISION_LIBRARY_DEBUG}" "${Bullet_DYNAMICS_LIBRARY_DEBUG}" "${Bullet_LINEAR_MATH_LIBRARY_DEBUG}")
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Bullet DEFAULT_MSG Bullet_ROOT_DIR Bullet_INCLUDE_DIR Bullet_LIBRARY_RELEASE Bullet_LIBRARY_DEBUG)
 else()
   # No linux support from Bullet!
 endif()
-
-set(Bullet_LIBRARY_RELEASE "${Bullet_COLLISION_LIBRARY_RELEASE}" "${Bullet_DYNAMICS_LIBRARY_RELEASE}" "${Bullet_LINEAR_MATH_LIBRARY_RELEASE}")
-set(Bullet_LIBRARY_DEBUG "${Bullet_COLLISION_LIBRARY_DEBUG}" "${Bullet_DYNAMICS_LIBRARY_DEBUG}" "${Bullet_LINEAR_MATH_LIBRARY_DEBUG}")
 mark_as_advanced(Bullet_COLLISION_LIBRARY_RELEASE)
 mark_as_advanced(Bullet_DYNAMICS_LIBRARY_RELEASE)
 mark_as_advanced(Bullet_LINEAR_MATH_LIBRARY_RELEASE)
@@ -52,9 +71,6 @@ mark_as_advanced(Bullet_LINEAR_MATH_LIBRARY_DEBUG)
 
 include(SelectConfigurations)
 select_configurations(Bullet LIBRARY LIBRARIES)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Bullet DEFAULT_MSG Bullet_ROOT_DIR Bullet_INCLUDE_DIR Bullet_LIBRARY_RELEASE Bullet_LIBRARY_DEBUG)
 
 include(CreateImportTargetHelpers)
 
