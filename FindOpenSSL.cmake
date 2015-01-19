52d3995d265571cd345201ae204bd5926ace2169
Walter Gray <wgray@leapmotion.com>
2015-01-19 13:18:57 -0800

    Consolidated & fixed up the FindOpenSSL module

--- FindOpenSSL.cmake
+++ FindOpenSSL.cmake
@@ -5,49 +5,39 @@ find_path(OPENSSL_ROOT_DIR
   PATH_SUFFIXES openssl
 )
 
-include(CreateImportTargetHelpers)
+include(${CMAKE_ROOT}/Modules/FindOpenSSL.cmake)
+
 
 if(MSVC)
-  find_library(LIB_EAY_DEBUG NAMES libeay32.lib HINTS ${OPENSSL_ROOT_DIR}/lib/debug)
-  find_library(LIB_EAY_RELEASE NAMES libeay32.lib HINTS ${OPENSSL_ROOT_DIR}/lib/release)
-  find_library(SSL_EAY_DEBUG NAMES ssleay32.lib HINTS ${OPENSSL_ROOT_DIR}/lib/debug)
-  find_library(SSL_EAY_RELEASE NAMES ssleay32.lib HINTS ${OPENSSL_ROOT_DIR}/lib/release)
-  mark_as_advanced(LIB_EAY_DEBUG LIB_EAY_RELEASE SSL_EAY_DEBUG SSL_EAY_RELEASE)
-
-  include(${CMAKE_ROOT}/Modules/FindOpenSSL.cmake)
-
+  find_library(OPENSSL_SSL_LIBRARY_DEBUG NAMES libeay32.lib HINTS ${OPENSSL_ROOT_DIR}/lib/debug)
+  find_library(OPENSSL_SSL_LIBRARY_RELEASE NAMES libeay32.lib HINTS ${OPENSSL_ROOT_DIR}/lib/release)
+  find_library(OPENSSL_CRYPTO_LIBRARY_DEBUG NAMES ssleay32.lib HINTS ${OPENSSL_ROOT_DIR}/lib/debug)
+  find_library(OPENSSL_CRYPTO_LIBRARY_RELEASE NAMES ssleay32.lib HINTS ${OPENSSL_ROOT_DIR}/lib/release)
+  mark_as_advanced(OPENSSL_SSL_LIBRARY_DEBUG OPENSSL_SSL_LIBRARY_RELEASE OPENSSL_CRYPTO_LIBRARY_DEBUG OPENSSL_CRYPTO_LIBRARY_RELEASE)
   #override the bad OPENSSL_LIBRARIES value
   include(SelectConfigurations)
-  select_configurations(LIB_EAY LIBRARY)
-  select_configurations(SSL_EAY LIBRARY)
+  select_configurations(OPENSSL_SSL LIBRARY LIBRARIES)
+  select_configurations(OPENSSL_CRYPTO LIBRARY LIBRARIES)
+endif()
   
-  add_library(OpenSSL::SSL STATIC IMPORTED)
-  set_target_properties(OpenSSL::SSL PROPERTIES IMPORTED_LOCATION_DEBUG ${LIB_EAY_DEBUG})
-  set_target_properties(OpenSSL::SSL PROPERTIES IMPORTED_LOCATION_RELEASE ${LIB_EAY_RELEASE})
+if(EXISTS "${OPENSSL_CRYPTO_LIBRARY_DEBUG}" OR EXISTS "${OPENSSL_CRYPTO_LIBRARY}")
+  set(OPENSSL_CRYPTO_FOUND TRUE)
+endif()
+if(EXISTS "${OPENSSL_SSL_LIBRARY_DEBUG}" OR EXISTS "${OPENSSL_SSL_LIBRARY}")
+  set(OPENSSL_SSL_FOUND TRUE)
+endif()
+if(OPENSSL_SSL_FOUND AND OPENSSL_CRYPTO_FOUND)
+  set(OPENSSL_FOUND TRUE)
+endif()
 
-  add_library(OpenSSL::Crypto STATIC IMPORTED)
-  set_target_properties(OpenSSL::Crypto PROPERTIES IMPORTED_LOCATION_DEBUG ${SSL_EAY_DEBUG})
-  set_target_properties(OpenSSL::Crypto PROPERTIES IMPORTED_LOCATION_RELEASE ${SSL_EAY_RELEASE})
+include(CreateImportTargetHelpers)
+generate_import_target(OPENSSL_SSL STATIC TARGET OpenSSL::SSL)
+generate_import_target(OPENSSL_CRYPTO STATIC TARGET OpenSSL::Crypto)
 
-  add_library(OpenSSL::OpenSSL INTERFACE IMPORTED GLOBAL)
-  target_link_libraries(OpenSSL::OpenSSL INTERFACE OpenSSL::SSL OpenSSL::Crypto)
-  set_target_properties(OpenSSL::OpenSSL PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INCLUDE_DIR})
+set(OPENSSL_INTERFACE_LIBS OpenSSL::SSL OpenSSL::Crypto)
+generate_import_target(OPENSSL INTERFACE TARGET OpenSSL::OpenSSL)
 
-else()
-  include(${CMAKE_ROOT}/Modules/FindOpenSSL.cmake)
-  if(EXISTS "${OPENSSL_CRYPTO_LIBRARY}")
-    set(OPENSSL_CRYPTO_FOUND TRUE)
-  endif()
-  if(EXISTS "${OPENSSL_SSL_LIBRARY}")
-    set(OPENSSL_SSL_FOUND TRUE)
-  endif()
-  
-  generate_import_target(OPENSSL_CRYPTO STATIC TARGET OpenSSL::Crypto)
+if(NOT MSVC)
   set_property(TARGET OpenSSL::Crypto APPEND PROPERTY INTERFACE_LINK_LIBRARIES -ldl)
-  
-  generate_import_target(OPENSSL_SSL STATIC TARGET OpenSSL::SSL)
   set_property(TARGET OpenSSL::SSL APPEND PROPERTY INTERFACE_LINK_LIBRARIES -ldl)
-
-  generate_import_target(OPENSSL INTERFACE TARGET OpenSSL::OpenSSL)
-  target_link_libraries(OpenSSL::OpenSSL INTERFACE OpenSSL::SSL OpenSSL::Crypto)
 endif()
